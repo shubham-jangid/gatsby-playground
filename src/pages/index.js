1dda6161b4531ccd53617d15f8c2714219a2727d
shubham jangid <contact@shubhamjangid.in>
2021-12-29 11:59:09 +0530

    Update index.js
    shubham jangid

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ const IndexPage = () => {
     <main className="main">
       <h1>Testing Gatsby</h1>
       <p>Gatsby is cool</p>
+     <h1> SHubham jangid</h1>
+      <p>Gatsby is cool</p>
     </main>
   )
 }
