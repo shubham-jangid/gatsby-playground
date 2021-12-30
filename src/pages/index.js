7d846dc05e58ce1f74294018f26ce6e8fed5e9d3
shubham jangid <contact@shubhamjangid.in>
2021-12-30 21:39:57 +0530

    Update index.js
    ads

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,24 @@ const IndexPage = () => {
       <h1>Testing Gatsby</h1>
       <p>Gatsby is cool</p>
      <h1> SHubham jangid</h1>
+      <p>Gatsby is cool</p>      <h1>Testing Gatsby</h1>
+      <p>Gatsby is cool</p>
+     <h1> SHubham jangid</h1>
+      <p>Gatsby is cool</p>      <h1>Testing Gatsby</h1>
+      <p>Gatsby is cool</p>
+     <h1> SHubham jangid</h1>
+      <p>Gatsby is cool</p>      <h1>Testing Gatsby</h1>
+      <p>Gatsby is cool</p>
+     <h1> SHubham jangid</h1>
+      <p>Gatsby is cool</p>      <h1>Testing Gatsby</h1>
+      <p>Gatsby is cool</p>
+     <h1> SHubham jangid</h1>
+      <p>Gatsby is cool</p>      <h1>Testing Gatsby</h1>
+      <p>Gatsby is cool</p>
+     <h1> SHubham jangid</h1>
+      <p>Gatsby is cool</p>      <h1>Testing Gatsby</h1>
+      <p>Gatsby is cool</p>
+     <h1> SHubham jangid</h1>
       <p>Gatsby is cool</p>
     </main>
   )
